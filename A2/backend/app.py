41c64f4d762337dec52f7a6248625924292cba65
rafaelkatalan <rafael.katalan@sou.inteli.edu.br>
2023-08-23 16:14:47 -0300

    erro

--- A2/backend/app.py
+++ A2/backend/app.py
@@ -1,9 +1,29 @@
 import psycopg2
 from flask import Flask, render_template, request, jsonify, redirect
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from sqlalchemy import create_engine
 import datetime
 
 app = Flask(__name__)
+jwt = JWTManager(app)
+# Set your Flask app's secret key
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+
+# Set your JWT secret key
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key_here'
+
+@jwt.expired_token_loader
+def handle_expired_token():
+    return redirect(('/loginPage'))
+
+@jwt.invalid_token_loader
+def handle_invalid_token():
+    return redirect(('/loginPage'))
+
+@app.errorhandler(401)
+def unauthorized(error):
+    # Redirect the user to the login page
+    return redirect(('/loginPage'))
 
 def connect_to_db():
     connection = psycopg2.connect(
@@ -15,14 +35,6 @@ def connect_to_db():
     )
     return connection
 
-def checkUser():
-    connection = connect_to_db()
-    cursor = connection.cursor()
-    user = 'userTeste'
-    cursor.execute(f"SELECT * FROM users WHERE username = '{user}'")
-    user = cursor.fetchone()
-    if user[2] == 'teste123':
-        return [True, user[0]]
 
 #@app.route('/')
 #def index():
@@ -47,47 +59,46 @@ def login():
     connection.close()
     if password == users[2]:
         print('Login realizado com sucesso')
-        return jsonify({'status': 'success'})
+        access_token = create_access_token(identity=users[0])
+        return redirect('/getnotes', code = 303)
     else:
         print('Login falhou')
-        return jsonify({'status': 'fail'})
+        return jsonify({'status': 'fail', 'message': 'Invalid credentials'}), 401
 
 @app.route('/newnotePage')
+@jwt_required()
 def newnotePage():
     return render_template('newNote.html')
 
 @app.route('/newnote', methods=['POST'])
+@jwt_required()
 def newnote():
-    user = checkUser()
-    if user[0]:
-        
-        connection = connect_to_db()
-        cursor = connection.cursor()
-        title = request.form.get('title')
-        content = request.form.get('content')
-        to_do_date = request.form.get('date')  # Replace this with the actual timestamp
-        print(title, content, to_do_date)
-        # Use parameterized query to prevent SQL injection
-        cursor.execute("INSERT INTO notes (title, content, to_do_date, user_id) VALUES (%s, %s, %s, %s)", (title, content, to_do_date, user[1]))
 
-        connection.commit()
-        connection.close()
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    title = request.form.get('title')
+    content = request.form.get('content')
+    to_do_date = request.form.get('date')  # Replace this with the actual timestamp
+    print(title, content, to_do_date)
+    # Use parameterized query to prevent SQL injection
+    cursor.execute("INSERT INTO notes (title, content, to_do_date, user_id) VALUES (%s, %s, %s, %s)", (title, content, to_do_date, get_jwt_identity()))
 
-        return redirect('/getnotes')
-    else:
-        return jsonify({'status': 'fail'})
+    connection.commit()
+    connection.close()
+
+    return redirect('/getnotes')
 
 @app.route('/getnotes', methods=['GET'])
+@jwt_required()
 def getnotes():
-    user = checkUser()
-    if user[0]:
-        connection = connect_to_db()
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT * FROM notes WHERE user_id = '{user[1]}'")
-        notes = cursor.fetchall()
-        print(notes)
-        connection.close()
-        return render_template('notes.html', notes=notes)
+    
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM notes WHERE user_id = '{get_jwt_identity()}'")
+    notes = cursor.fetchall()
+    print(notes)
+    connection.close()
+    return render_template('notes.html', notes=notes)
 
 
 if __name__ == '__main__':
