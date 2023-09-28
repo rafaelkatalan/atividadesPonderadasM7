5eacabea93f747babc3b5654237fdd8199f0df70
rafaelkatalan <rafael.katalan@sou.inteli.edu.br>
2023-09-28 14:22:09 -0300

    final

--- A4/backend/app.py
+++ A4/backend/app.py
@@ -8,10 +8,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 import base64
+#para usar o modelo real importe a biblioteca pycaret com o comando a baixo:
 #from pycaret.classification import load_model, predict_model
 
 
-# Load the model
+#para usar o modelo real execute a linha a baixo
 #modelo = joblib.load('lgbmregressor.pkl')
 
 
@@ -88,12 +89,15 @@ def inputData():
             utilitarios = request.form['utilitarios']
             hora = request.form['hora']
 
+            #para usar o modelo real descomente as linhas a baixo e comente a linha 98
             #dados = [[km, automovel, bicicleta, caminhao, moto, onibus, outros, tracao_animal, transporte_de_cargas_especiais, trator_maquinas, utilitarios, prediction_mortos, hora]]
             #resultado = modelo.predict(dados)
+            # O resultado é a previsão para 'prediction_mortos'
+            #resultado = resultado[0]
+
             resultado = random.randint(0, 10)
 
-            # O resultado é a previsão para 'prediction_mortos'
-            prediction_mortos = resultado
+
 
             # Conecta-se ao banco de dados PostgreSQL
             connection = connect_to_db()
@@ -154,44 +158,30 @@ def dashboards():
         connection.close()
         column_names1 = ['hora', 'mortos_rawdata', 'km_rawdata']
         column_names2 = ['hora', 'prediction_mortos_predictions', 'km_predictions']
-        # Convert the results into a pandas DataFrame
-        #df = pd.DataFrame(columns=['hora', 'mortos_rawdata', 'km_rawdata', 'prediction_mortos_predictions', 'km_predictions'])
+        
         df = pd.DataFrame(data1, columns=column_names1)
         df2 = pd.DataFrame(data2, columns=column_names2)
         print(df.columns)
-
-        #x = df['mortos_rawdata'][0]
-        #df['hour'] = pd.to_datetime(df['hora']).dt.hour
-        #df['minute'] = pd.to_datetime(df['hora']).dt.minute
-        #df['second'] = pd.to_datetime(df['hora']).dt.second
-
-        #df['hora'] = df['hour'] + df['minute']/60 + df['second']/3600
-
 
         plt.plot(df['hora'], df['mortos_rawdata'], label='Mortos')
         plt.xlabel('hora')
         plt.ylabel('mortos_rawdata')
 
-            # Save the first plot to a buffer
         buffer1 = io.BytesIO()
         plt.savefig(buffer1, format='png')
         buffer1.seek(0)
 
-        # Encode the first plot as a base64 string
         plot_data1 = base64.b64encode(buffer1.read()).decode()
         plt.clf()
         plt.plot(df2['hora'], df2['prediction_mortos_predictions'], label='Prediction')
         plt.xlabel('hora')
         plt.ylabel('prediction_mortos_predictions')
-
-            # Save the second plot to a buffer 
+ 
         buffer2 = io.BytesIO()
         plt.savefig(buffer2, format='png')
         buffer2.seek(0)
 
-        # Encode the second plot as a base64 string
         plot_data2 = base64.b64encode(buffer2.read()).decode()
-
 
         return render_template('dashboard.html', plot_data1=plot_data1, plot_data2=plot_data2)
     else:
